e05718d9b3a6066a02e336f88ee861a6ea27b1b0
Omar Almalol <omar.alma3lol@gmail.com>
2020-05-10 08:40:24 +0200

    fix(decorators): remove unused code & add metadata keys

--- src/Types/Decorators.Definitions.ts
+++ src/Types/Decorators.Definitions.ts
@@ -1,55 +1,14 @@
-import { PropertyDefinition } from './Models.Definitions';
-import { MetadataAccessor } from './Decorators.Classes';
 /**
-* Decorator function types
-*/
-export type DecoratorType =
-	| ClassDecorator
-	| PropertyDecorator
-	| MethodDecorator
-	| ParameterDecorator
-
-/**
-* Key for metadata access via reflection
-* @typeParam T - Type of the metadata value
-* @typeParam D - Type of the decorator
-*/
-export type MetadataKey<T, D extends DecoratorType> =
-	| MetadataAccessor<T, D>
-	| string;
-
-/**
-* An object mapping keys to corresponding metadata
-*/
-export interface MetadataMap<T> {
-	[propertyOrMethodName: string]: T;
-}
-
-/**
- * Design time metadata for a method.
- *
+ * class table metadata key
  */
-export interface DesignTimeMethodMetadata {
-	/**
-	* Type of the method itself. It is `Function`
-	*/
-	type: Function;
-	/**
-	* An array of parameter types
-	*/
-	parameterTypes: Function[];
-	/**
-	* Return type
-	*/
-	returnType: Function;
-}
+export const CLASS_TABLE_METADATA_KEY = "class-table";
 
 /**
- * Options for a decorator
+ * class api path metadata key
  */
-export interface DecoratorOptions {
-	/**
-	 * Name of the decorator for debugging purpose, such as `@inject`
-	 */
-	decoratorName?: string;
-}
+export const CLASS_API_PATH_METADATA_KEY = "class-api-path";
+
+/**
+ * class id property metadata key
+ */
+export const CLASS_ID_PROPERTY_METADATA_KEY = "class-id-property";
--- src/Types/Decorators.ts
+++ src/Types/Decorators.ts
@@ -1,5 +1,2 @@
 export * from './Decorators.Definitions';
-export * from './Decorators.Factories';
-export * from './Decorators.Classes';
 export * from './Decorators.Reflectors';
-export * from './Decorators.Inspectors';

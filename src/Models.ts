8b764d7816e86318fc86b0f07d9f209f67ec8608
Omar Almalol <omar.alma3lol@gmail.com>
2020-05-10 10:47:23 +0200

    feat(models): add getTable/getApiPath

--- src/Models.ts
+++ src/Models.ts
@@ -1,7 +1,10 @@
-import { AnyObject, Reflector, CLASS_ID_PROPERTY_METADATA_KEY } from './Types';
+import { AnyObject, Reflector, CLASS_ID_PROPERTY_METADATA_KEY, CLASS_TABLE_METADATA_KEY, CLASS_API_PATH_METADATA_KEY } from './Types';
 
-export class Model {
+export abstract class Model {
 	[name: string]: any
+	constructor(data: Partial<Model>) {
+		Object.assign(this, data)
+	}
 	Deserialize(): Object {
 		const data: AnyObject = {};
 		for (const prop in this) {
@@ -14,6 +17,20 @@ export class Model {
 			return this[Reflector.getMetadata(CLASS_ID_PROPERTY_METADATA_KEY, this)]
 		} else {
 			throw Error("No ID property set using '@property' decorator!")
+		}
+	}
+	static getTable(target: any = this) {
+		if (Reflector.hasMetadata(CLASS_TABLE_METADATA_KEY, target)) {
+			return Reflector.getMetadata(CLASS_TABLE_METADATA_KEY, target)
+		} else {
+			throw Error("No table set using '@model' decorator!")
+		}
+	}
+	static getApiPath(target: any = this) {
+		if (Reflector.hasMetadata(CLASS_API_PATH_METADATA_KEY, target)) {
+			return Reflector.getMetadata(CLASS_API_PATH_METADATA_KEY, target)
+		} else {
+			throw Error("No api path set using '@model' decorator!")
 		}
 	}
 }

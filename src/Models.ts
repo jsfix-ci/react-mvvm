fb2616f3255b3683b3f4c132ab05d6e3925dd339
Omar Almalol <omar.alma3lol@gmail.com>
2020-05-10 08:43:12 +0200

    fix(models): apply new changes & rename idProperty to getId

--- src/Models.ts
+++ src/Models.ts
@@ -1,16 +1,19 @@
-import { AnyObject } from './Types';
+import { AnyObject, Reflector, CLASS_ID_PROPERTY_METADATA_KEY } from './Types';
 
 export class Model {
 	[name: string]: any
 	Deserialize(): Object {
 		const data: AnyObject = {};
 		for (const prop in this) {
-			if (typeof this[prop] !== 'function' && prop !== 'decoratorIdProperty') data[prop] = this[prop]
+			if (typeof this[prop] !== 'function') data[prop] = this[prop]
 		}
 		return data
 	}
-	idProperty = () => {
-		if (this.decoratorIdProperty) return this[this.decoratorIdProperty]
-		throw Error("No ID property set using '@property' decorator!")
+	getId() {
+		if (Reflector.hasMetadata(CLASS_ID_PROPERTY_METADATA_KEY, this)) {
+			return this[Reflector.getMetadata(CLASS_ID_PROPERTY_METADATA_KEY, this)]
+		} else {
+			throw Error("No ID property set using '@property' decorator!")
+		}
 	}
 }

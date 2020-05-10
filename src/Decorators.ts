eb68a7c1c03d20c92073cba6fedfc43a330e3164
Omar Almalol <omar.alma3lol@gmail.com>
2020-05-10 08:41:44 +0200

    feat(decorators): clear code & add functionality to model decorator

--- src/Decorators.ts
+++ src/Decorators.ts
@@ -1,74 +1,15 @@
-import { ModelDefinition, ModelDefinitionSyntax, ClassDecoratorFactory, PropertyDefinition, MetadataMap, MetadataAccessor, PropertyDecoratorFactory } from './Types'
-import { MetadataInspector } from './Types/Decorators.Inspectors';
-
-export const MODEL_KEY = MetadataAccessor.create<
-	Partial<ModelDefinitionSyntax>,
-	ClassDecorator
->('react-mvvm:model');
-export const MODEL_PROPERTIES_KEY = MetadataAccessor.create<
-	PropertyDefinition,
-	PropertyDecorator
->('react-mvvm:model-properties');
-export const MODEL_WITH_PROPERTIES_KEY = MetadataAccessor.create<
-	ModelDefinition,
-	ClassDecorator
->('react-mvvm:model-and-properties');
-
-export type PropertyMap = MetadataMap<PropertyDefinition>;
+import { ModelDefinition, PropertyDefinition, Reflector, CLASS_ID_PROPERTY_METADATA_KEY, CLASS_TABLE_METADATA_KEY, CLASS_API_PATH_METADATA_KEY } from './Types'
 
 /**
 * Decorator for model definitions
 * @param definition
 * @returns A class decorator for `model`
 */
-export function model(definition?: Partial<ModelDefinitionSyntax>) {
-	return function (target: Function & { definition?: ModelDefinition }) {
-		definition = definition ?? {};
-		const def: ModelDefinitionSyntax = Object.assign(definition, {
-			name: definition.name ?? target.name,
-		});
-		const decorator = ClassDecoratorFactory.createDecorator(
-			MODEL_KEY,
-			definition,
-			{ decoratorName: '@model' },
-		);
-
-		decorator(target);
-
-		// Build "ModelDefinition" and store it on model constructor
-		buildModelDefinition(target, def);
-	};
-}
-
-/**
-* Build model definition from decorations
-* @param target - Target model class
-* @param def - Model definition spec
-*/
-export function buildModelDefinition(
-	target: Function & { definition?: ModelDefinition },
-	def?: ModelDefinitionSyntax,
-) {
-	// Check if the definition for this class has been built (not from the super
-	// class)
-	const baseClass = Object.getPrototypeOf(target);
-	if (
-		!def &&
-		target.definition &&
-		baseClass &&
-		target.definition !== baseClass.definition
-	) {
-		return target.definition;
+export const model = (definition: ModelDefinition) => {
+	return function (target: Function) {
+		Reflector.defineMetadata(CLASS_TABLE_METADATA_KEY, definition.table, target);
+		Reflector.defineMetadata(CLASS_API_PATH_METADATA_KEY, definition.api_path, target);
 	}
-	const modelDef = new ModelDefinition(def ?? { name: target.name });
-	const prototype = target.prototype;
-	const propertyMap: PropertyMap =
-		MetadataInspector.getAllPropertyMetadata(MODEL_PROPERTIES_KEY, prototype) ?? {};
-	for (const p in propertyMap) {
-		modelDef.addProperty(p, propertyMap[p]);
-	}
-	target.definition = modelDef;
-	return modelDef;
 }
 
 /**
@@ -76,10 +17,44 @@ export function buildModelDefinition(
 * @param definition
 * @returns A property decorator
 */
-export function property(definition?: Partial<PropertyDefinition>) {
-	return PropertyDecoratorFactory.createDecorator(
-		MODEL_PROPERTIES_KEY,
-		Object.assign({}, definition),
-		{ decoratorName: '@property' },
-	);
+export const property = (definition: PropertyDefinition) => {
+	return (target: any, propertyName: string) => {
+		if (definition.id) {
+			if (!Reflector.hasMetadata(CLASS_ID_PROPERTY_METADATA_KEY, target)) {
+				Reflector.defineMetadata(CLASS_ID_PROPERTY_METADATA_KEY, propertyName, target);
+			} else {
+				throw Error("A model can only have a single ID property!")
+			}
+		}
+	}
+}
+
+/**
+ * Decorator for model methods
+ * @param definition
+ * @returns A method decorator
+ */
+export const method = (definition?: any) => {
+	return (
+		target: Object,
+		methodName: string | symbol,
+		descriptor: TypedPropertyDescriptor<any>,
+	) => {
+		// TODO: WHAT HAPPENS HERE
+	}
+}
+
+/**
+ * Decorator for methods' parameters
+ * @param definition
+ * @returns A parameter decorator
+ */
+export const parameter = (definition?: any) => {
+	return (
+		target: Object,
+		methodName: string | symbol,
+		parameterIndex: number,
+	) => {
+		// TODO: WHAT HAPPENS HERE
+	}
 }
